f8c026c66d76d34a0a2ac599df203620cb87d594
Adrian_Bugarin <adrianbugarin3@gmail.com>
2021-11-12 12:18:05 -0700

    -m

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
 // 1
-let value = "False"
+function addToZero() {
 for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
     if (i !== j) {
       if (array[i] + array[j] === 0) {
-        value = "True"
+        return('true')
       } 
     }
+    }
+} return('false')
   }
-}
-console.log(value)
 
 // 2
-function uniqueletter(string) {
-    var strLength = string.length;
-    var arr = [];
-    for (var i = 0; i<strLength; i++){
-        var subStr = string.substring(i,i+1);
-        
-        if(arr.find(function(letter) {return letter === subStr})){
-            return(false);
-        };
+function uniqueletter(str) {
+    let letters = {}
 
-        arr[i] = subStr;
+    for (var i = 0; i<str.Length; i++){
+        if (letters[str[i]]) {
+            return false
+        } else {
+            letters[str[i]] = 1
+        }
     }
-    return(true);
-}
-var result = uniqueletter(letters);
+    return true
+  }
 
 // 3
-function pangrams(s) {
+function pangrams(str) {
     let alphabet = "abcdefghijklmnopqrstuvwxyz"
     let regex = /\s/g;
-    let lowercase = s.toLowerCase().replace(regex, "")
+    let lowercase = str.toLowerCase().replace(regex, "")
    
     for(let i = 0; i < alphabet.length; i++){
      if(lowercase.indexOf(alphabet[i]) === -1){
-       return(false);
+       return false
      }
     }
    
-   return(true);
+   return true
+ }
+
+
+ function isPangram(str) {
+     str = str.toLowerCase().replace(/([*a-z])*/g, ``)
+     
+     let letters = {}
+
+     for (let i =0; i < str.length; i++) {
+         if (!letters.hasOwnProperty(str[i])) {
+             letters[str[i]] = 1
+         }
+     }
+     if (Object.keys(letters))
  }
 
 //4
+longestWord = ('hello', 'world')
+
 function findLongestWord(str) {
-    var strSplit = str.split(' ');
+    var strSplit = str.split(' ')
     var longestWord = 0;
+
     for(var i = 0; i < strSplit.length; i++){
+
       if(strSplit[i].length > longestWord){
-      longestWord = strSplit[i].length;
+      longestWord = strSplit[i].length
        }
     }
-    return longestWord;
+    return longestWord
   }
-  findLongestWord("hi", "hello");
